aefed9190b5a39141753a13b0116ff6c419e484b
shoummo <76985708+shoummo@users.noreply.github.com>
2021-01-05 09:51:59 +0530

    Add files via upload

---
+++ Functions.R
@@ -0,0 +1,39 @@
+### Function 1: makeCacheMatrix
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+  set <- function(y) {
+          x <<- y
+          i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+### Function 2: cacheSolve
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+          message("getting cached data")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+
+### RUN
+#1. Creating a matrix
+set.seed(1)
+x <- matrix(rnorm(25),5,5)
+
+#2. Running the functions
+x1 <- makeCacheMatrix(x)
+cacheSolve(x1)
